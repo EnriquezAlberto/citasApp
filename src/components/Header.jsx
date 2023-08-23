5ee5e89f3691215da81d965b14fee8947853945d
Alberto Enriquez <136874006+EnriquezAlberto@users.noreply.github.com>
2023-08-23 19:09:38 -0300

    sintaxis mejorada

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,4 @@
- const Header = () => {
-
-  
-
+const Header = () => {
   return (
     <>
       <h1 className="font-black text-5xl text-center md:w-1/2 mx auto">
@@ -12,5 +9,4 @@
   );
 };
 
-
-export default Header
+export default Header;
